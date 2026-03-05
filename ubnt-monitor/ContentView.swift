c55f324ccc4e1fab46b63ecbd5606bfb36875149
linxuchuan <linxuchuan19970926@163.com>
2026-03-05 14:47:15 +0800

    Initial Commit

---
+++ ubnt-monitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ubnt-monitor
+//
+//  Created by 川宝 on 2026/3/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ubnt-monitor/ubnt_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ubnt_monitorApp.swift
+//  ubnt-monitor
+//
+//  Created by 川宝 on 2026/3/5.
+//
+
+import SwiftUI
+
+@main
+struct ubnt_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
